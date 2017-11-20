8701a354b67ce9b32fc9701189168d56855e5731
jay.yu <jay.yu@jame.me>
2017-11-20 18:13:21 +0900

    feed export 인코딩 추가

--- coin_crawler/settings.py
+++ coin_crawler/settings.py
@@ -87,3 +87,5 @@ DOWNLOAD_DELAY = 0.2
 # HTTPCACHE_DIR = 'httpcache'
 # HTTPCACHE_IGNORE_HTTP_CODES = []
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+FEED_EXPORT_ENCODING = 'utf-8'
